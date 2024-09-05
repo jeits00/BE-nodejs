ae2d49f48bffca984ce47386bacaf09dca8819dd
jeits00 <vanthanh031102@gmail.com>
2024-09-05 15:18:00 +0700

    push new

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require("dotenv").config();
+
+const database = require("./config/database");
+
+const route = require("./routes/client/index.route");
+
+database.connect();
+
+const app = express()
+const port = 3000
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+
+// Routes
+route(app);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
