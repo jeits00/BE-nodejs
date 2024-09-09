583a7d759f4763600ece5add6f31fe45e691e564
jeits00 <vanthanh031102@gmail.com>
2024-09-09 22:09:28 +0700

    3. Tính năng lọc trạng thái

--- controller/adimin/product.controller.js
+++ controller/adimin/product.controller.js
@@ -2,15 +2,47 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/product
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        deleted: false 
-    });
+    let filterStatus = [
+    {
+        name: "Tat ca",
+        status: "",
+        class: "active"
+    },
+    {
+        name: "Hoat Dong",
+        status: "active",
+        class: ""
+    },
+    {
+        name: "Dung hoat dong",
+        status: "inactive",
+        class: ""
+    }
+    ]
 
-    console.log(products);
+    if(req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus[index].class = "active";
+    } else {
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+    }
+
+    let find = {
+        deleted: false
+    };
+
+    if(req.query.status) {
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
+
+    // console.log(products);
   
     res.render("admin/pages/products/index", {
         pageTitle: "Danh Sach San Pham",
-        products: products
- 
+        products: products,
+        filterStatus: filterStatus
     });
 }
