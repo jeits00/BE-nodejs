d85781665f15f0835e50ae5268ca7ff1d9277cf6
jeits00 <vanthanh031102@gmail.com>
2024-09-09 16:30:42 +0700

    Làm giao diện danh sách sản phẩm

--- routes/adimin/index.router.js
+++ routes/adimin/index.router.js
@@ -1,9 +1,12 @@
 const systemConfig = require("../../config/system");
 
 const dashboardRouters = require("./dashboard.router");
+const dashboardRouters = require("./product.router");
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
 
-    app.use(PATH_ADMIN + "/admin/dashboard", dashboardRouters);
+    app.use(PATH_ADMIN + "/dashboard", dashboardRouters);
+
+    app.use(PATH_ADMIN + "/products", dashboardRouters);
 }
