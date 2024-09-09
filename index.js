7ef996264f4c2cf22aae3f80ec1c45b3f3de47a2
jeits00 <vanthanh031102@gmail.com>
2024-09-09 15:38:59 +0700

    1.2 Xây dựng layout chung

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
 const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
 
 database.connect();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// App Locals Variables 
+app.locals.prefixAdmin = systemConfig.prefixAdmin; 
 
 app.use(express.static("public"));
 
