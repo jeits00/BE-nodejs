d48ae2bc5f82ec568135663468df0dbd702b287a
jeits00 <vanthanh031102@gmail.com>
2024-09-13 16:17:43 +0700

    Cấu hình database

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// main().catch(err => console.log(err));
+
+module.exports.connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connect Success!");
+    } catch (error) {
+        console.log("Connect Error!");
+    }
+}
