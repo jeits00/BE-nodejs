412b39496898fc2e5ef32f6f6b3b88b79aea83a7
jeits00 <vanthanh031102@gmail.com>
2024-09-16 21:31:51 +0700

    20- 3.Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,14 +2,49 @@ const Product = require("../../models/product.model");
 
 // [GET] admin/products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        deleted: false
-    });
+    //  console.log(req.query.status);
 
-    console.log(products);
+    let filterStatus = [
+        {
+            name: "All",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Online",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Off",
+            status: "inactive",
+            class: ""
+        }
+    ];
+    
+    if(req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus[index].class = "active";
+    } else {
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+    }   
+
+    let find = {
+        deleted: false
+    };
+
+    if(req.query.status) {
+        find.status = req.query.status ;  
+    }
+
+    const products = await Product.find(find);
+
+    // console.log(products);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Product page ",
-        products: products
+        products: products,
+        filterStatus: filterStatus
     });
 }
