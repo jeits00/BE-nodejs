f47143e9225aee807e31136d66fbe6a2dd30fdf0
jeits00 <vanthanh031102@gmail.com>
2024-09-22 00:17:05 +0700

    17- 2.Tính năng phân trang (tối ưu)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,8 +1,8 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
-
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] admin/products
 module.exports.index = async (req, res) => {
@@ -26,25 +26,19 @@ module.exports.index = async (req, res) => {
     }
 
     // Pagination
-    let objectPagination = {
+    const countProducts = await Product.countDocuments(find);
+
+    let objectPagination = paginationHelper(
+        {
         currentPage: 1,
         limitItems: 4
-    };
-
-    if(req.query.page) {
-        objectPagination.currentPage = parseInt(req.query.page);
-    }
-
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
-    const countProducts = await Product.countDocuments(find);
-    const totalPage = Math.ceil(countProducts/objectPagination.limitItems);
-    objectPagination.totalPage = totalPage;
+        },
+        req.query,
+        countProducts
+    );
     // End Pagination
 
     const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
-
-    // console.log(products);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Product page ",
