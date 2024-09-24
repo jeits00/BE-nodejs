3a63d62b673116571f6827ba28c04bf1d7a69a61
jeits00 <vanthanh031102@gmail.com>
2024-09-24 17:13:09 +0700

    17- 4.1 Xóa Vĩnh viễn

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -78,3 +78,12 @@ module.exports.changeMulti = async (req, res) => {
 
     res.redirect("back");
 };
+
+// [PATCH] admin/products/delete/:id
+module.exports.deleteItem = async (req, res) => {
+    const id = req.params.id;
+
+    await Product.deleteOne({ _id: id });
+
+    res.redirect("back");
+};
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -18,5 +18,28 @@ if(buttonsChangeStatus.length > 0) {
         });
     });
 }
-
 // Change Status
+
+// Delete Item - Code tính năng xóa sp
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+if(buttonsChangeStatus.length > 0) {
+    const formDeleteItem = document.querySelectorAll("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+
+    buttonsChangeStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Are you sure you want to delete this product?");
+
+            if(isConfirm) {
+                const id = button.getAttribute("data-id");
+
+                const active = `${path}/${id}?_method=DELETE`;
+
+                formDeleteItem.action = active;
+
+                formDeleteItem.submit();
+            }
+        });
+    });
+} 
+// End Delete Item 
