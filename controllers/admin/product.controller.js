c05a707bc04ad540c424d8015123dae1cd07eb72
jeits00 <vanthanh031102@gmail.com>
2024-10-01 16:39:14 +0700

    18- 4. Tính năng tạo mới sản phẩm (upload ảnh)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -129,6 +129,7 @@ module.exports.create = async (req, res) => {
 
 // [GET] admin/products/createPost , tạo mới sản phẩm 
 module.exports.createPost = async (req, res) => {
+    console.log(req.file)
     req.body.price = parseInt(req.body.price);
     req.body.discountPercentage = parseInt(req.body.discountPercentage);
     req.body.stock = parseInt(req.body.stock);
@@ -139,6 +140,8 @@ module.exports.createPost = async (req, res) => {
     } else {
         req.body.position = parseInt(req.body.position);
     }
+
+    req.body.thumbnail = `/uploads/${req.file.filename}`;
 
     const product = new Product(req.body);
     await product.save();
