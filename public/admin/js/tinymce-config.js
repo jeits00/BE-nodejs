50683408126d79b9252f879e774d167015d19e07
jeits00 <vanthanh031102@gmail.com>
2024-10-24 14:51:42 +0700

    reset

--- public/admin/js/tinymce-config.js
+++
@@ -1,4 +0,0 @@
-tinymce.init({
-    selector: 'textarea.textarea-mce',
-    plugins: 'lists link image table code help wordcount'
-  });
--- routes/client/index.route.js
+++
@@ -1,8 +0,0 @@
-const homeRoutes = require("./home.route");
-const productsRoutes = require("./product.route");
-
-module.exports = (app) => {
-    app.use("/", homeRoutes);
-    
-    app.use("/products", productsRoutes);
-}
