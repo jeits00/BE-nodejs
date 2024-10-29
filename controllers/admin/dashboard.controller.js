d7c863b1e19ce3f585608a13922a9b57b50c7eed
jeits00 <vanthanh031102@gmail.com>
2024-10-29 21:43:58 +0700

    21- 3.2. và 3.3 Xây dựng phần Danh sách nhóm quyền

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,3 +1,6 @@
+// [GET] /admin/dashboard
 module.exports.dashboard = (req, res) => {
-    res.render("admin/pages/dashboard/index");
+    res.render("admin/pages/dashboard/index", {
+        pageTitle: "Overview page"
+    });
 }
