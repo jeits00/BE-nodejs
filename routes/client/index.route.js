00dffc45f8ca5fe9a0aac46ddfd59fff08065c2a
jeits00 <vanthanh031102@gmail.com>
2024-11-08 13:54:32 +0700

    24- 2. Hiển thị menu chung bên client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
+const categoryMiddleware = require("../../middlewares/client/category.middlewares");
+
 const homeRoute = require("./home.route");
 const productRoute = require("./product.route");
 
 module.exports = (app) => {
+    app.use( categoryMiddleware.category)
+
     app.use("/", homeRoute);
 
     app.use("/products", productRoute);
