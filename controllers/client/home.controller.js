cef74bc003ddb3d18d538696d934406a2042191a
jeits00 <vanthanh031102@gmail.com>
2024-11-10 14:12:49 +0700

    .section-products-featured.my-5          .container              .row                  .col-12                      +box-head("Sản phẩm nổi bật")              +product-grid(productsFeatured)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -9,13 +9,23 @@ module.exports.index = async (req, res) => {
         featured: "1",
         deleted: false, 
         status: "active"
-    });
+    }).limit(6);
 
-    const newProducts = productsHelper.priceNewProducts(productsFeatured);
+    const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
     // End Lấy ra sản phẩm nổi bật
+
+    // Hiển thị ra sản phẩm mới nhất 
+    const productsNew = await Product.find({
+        deleted: false, 
+        status: "active"
+    }).sort({ position: "desc" }).limit(6);
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew);
+    // End Hiển thị ra sản phẩm mới nhất    
 
     res.render("client/pages/home/index", {
         pageTitle: "Home",
-        productsFeatured: newProducts
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     });
 }
