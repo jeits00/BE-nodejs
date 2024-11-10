4597dd195d7de694a6ad2630ac18564e0672fa7b
jeits00 <vanthanh031102@gmail.com>
2024-11-10 13:37:09 +0700

    4.1. Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product.model");
 
+const productsHelper = require("../../helpers/products");
+
 // [GET] /products 
 module.exports.index = async (req, res) => {
     const products = await Product.find({
@@ -7,14 +9,11 @@ module.exports.index = async (req, res) => {
         deleted: false
     }).sort({ position: "desc" });
 
-    const newProducts = products.map(item => {
-        item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
-        return item;
-    });
+    const newProducts = productsHelper.priceNewProducts(products);
 
     res.render("client/pages/products/index", { 
         pageTitle: "Product list page",
-        products: products
+        products: newProducts
     })
 }
 
