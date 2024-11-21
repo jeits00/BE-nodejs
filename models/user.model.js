432c80fdbcd2a0a5f8211fce3a97b5be69e2bd07
jeits00 <vanthanh031102@gmail.com>
2024-11-21 17:47:15 +0700

    2.1. (sửa) Tạo collection users

--- models/user.model.js
+++ models/user.model.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 const generate = require("../helpers/generate");
 
-const accountSchema = new mongoose.Schema({ 
+const userSchema = new mongoose.Schema({ 
     fullName: String,
     email: String,
     password: String,
-    token: {
+    tokenUser: {
         type: String,
         default: generate.generateRandomString(20)
     },
     phone: String,
     avatar: String,
-    role_id: String,
-    status: String,
+    status: {
+        type: String,
+        default: "active"
+    },
     deleted: {
         type: Boolean,
         default: false,
@@ -22,6 +24,6 @@ const accountSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const Account = mongoose.model("Account", accountSchema, "accounts");
+const User = mongoose.model("User", userSchema, "users");
 
-module.exports = Account;
+module.exports = User;
